07684240ff34c178129a8d4b84f8e4679af5d481
mattperkins <github@mattperkins.com>
2018-10-22 19:56:57 +0100

    dry refactor

--- client/src/public/js/main.js
+++ client/src/public/js/main.js
@@ -1,6 +1,6 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
+const { render } = require('react-dom')
 const App = require('./components/app/App')
 
-ReactDOM.render(<App />, 
+render(<App />, 
   document.getElementById('root'))
