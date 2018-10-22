1896761cb0a26ecd69eadc765a392600e770930d
mattperkins <github@mattperkins.com>
2018-10-22 16:14:18 +0100

    rebuild

--- client/src/public/js/main.js
+++ client/src/public/js/main.js
@@ -1,7 +1,6 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
-
-const App = () => <h1>Hello, World!</h1>
+const App = require('./components/app/App')
 
 ReactDOM.render(<App />, 
   document.getElementById('root'))
