ead3d51693f35dd1a82b5932c75ce5839c55c4e3
mattperkins <github@mattperkins.com>
2018-10-23 09:53:36 +0100

    refactor

--- client/src/public/js/components/header/Header.js
+++
@@ -1,9 +0,0 @@
-const React = require('react')
-
-module.exports = () => {
- return(
-  <nav>
-   Header 
-  </nav>
- )
-}
--- client/src/public/js/main.js
+++
@@ -1,6 +0,0 @@
-const React = require('react')
-const { render } = require('react-dom')
-const App = require('./components/app/App')
-
-render(<App />, 
-  document.getElementById('root'))
