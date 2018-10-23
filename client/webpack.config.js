016e3df8108f81cc8570c7acf4404fd08aa6b810
mattperkins <github@mattperkins.com>
2018-10-23 21:06:23 +0100

    add postcss

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
   path: path.join(__dirname, "dist"),
   filename: "bundle.js"
  },
+ resolve: {
+  extensions: [".jsx", ".js"]
+ },
  mode: "development",
  module: {
   rules: [
