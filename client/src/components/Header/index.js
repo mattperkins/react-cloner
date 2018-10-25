fc28d15391f3e712577898c0d672b19301fa4104
mattperkins <github@mattperkins.com>
2018-10-25 23:08:07 +0100

    update

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,7 +3,7 @@ const React = require('react')
 module.exports = () => {
  return(
   <div className="header">
-   <p className="header__logo">DORMshed!</p>
+   <p className="header__logo">DORMshed</p>
   </div>
  )
 }
