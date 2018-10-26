0a2ebf3f87a9de9692f7e0dbbd1fe507f6d4b443
mattperkins <github@mattperkins.com>
2018-10-26 19:22:18 +0100

    update

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 
 app.get('/*', (req,res) => {
- res.send('hello, world!')
+ res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
 app.listen(3000, () => console.log(':3000'))
