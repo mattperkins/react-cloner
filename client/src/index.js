adb53a71a2db1c87dcb3c147fd2aeb63218890cf
mattperkins <github@mattperkins.com>
2018-10-26 09:02:33 +0100

    add root component

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+import 'index.css'
+
+class Root extends React.Component{
+ render(){
+  return(
+   <h1>Hello, World!</h1>
+  )
+ }
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'))
