2f1033e2a6038b3e923b21f5113cc0a4d54772b6
mattperkins <github@mattperkins.com>
2018-11-21 15:04:38 +0000

    format to standard

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import * as React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 
-/* webpackChunkName: 'app' */ 
-import('./components/App')
+import (/* webpackChunkName: 'app' */ `./components/App`)
  .then(({ default: App }) => 
  render(<App />, 
  document.getElementById('root'))
