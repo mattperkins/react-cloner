9f23e3c916398d2aa65129970d96b33b7fb15219
mattperkins <github@mattperkins.com>
2018-11-22 19:54:27 +0000

    configure rules for css and paths (to avoid react-router errors)

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
   devtool: 'cheap-module-source-map',
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
     ]
+  },
+  devServer: {
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
